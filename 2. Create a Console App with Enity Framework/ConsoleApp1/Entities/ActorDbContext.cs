a859760354923650b237948d6efcb2db546ac911
Daniel C <danielcoco96@gmail.com>
2024-01-25 14:32:05 +0100

    2. Create a Console App with Enity Framework

---
+++ 2. Create a Console App with Enity Framework/ConsoleApp1/Entities/ActorDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1.Entities
+{
+    public class ActorDbContext : DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ActorDb;"
+                                        + "Trusted_Connection=True");
+        }
+    }
+}
---
+++ 2. Create a Console App with Enity Framework/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1.Entities;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ActorDbContext db = new ActorDbContext())
+            {
+                // Seed
+                db.Actors.AddRange(
+                    new Actor 
+                    {
+                        Name = "Bruce Lee",
+                        Age = 75,
+                        AcademyWinner = false
+                    }, 
+                    new Actor
+                    {
+                        Name = "Madonna",
+                        Age = 55,
+                        AcademyWinner = true
+                    });
+
+                var count = db.SaveChanges();
+
+                // Fetch data
+                Console.WriteLine($"{count} records added");
+
+                foreach ( var actor in db.Actors ) {
+                    Console.WriteLine($"Name - {actor.Name},\t\t" +
+                        $"Age: {actor.Age},\t\t" +
+                        $"Academy Winner: {actor.AcademyWinner}");
+                }
+
+                Console.ReadLine();
+
+            }
+
+
+        }
+    }
+}
